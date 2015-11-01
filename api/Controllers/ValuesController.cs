5bc1e89142d4d13b852dde92f4938ea1ba474667
litealex <litealex@gmail.com>
2015-11-01 13:36:23 +0300

    values ctrl

---
+++ api/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+namespace api
+{
+	public class ValuesController:ApiController
+	{
+		public async Task<object> Get(){
+			return await Task.FromResult ("value1");
+		}
+	}
+}
+
---
+++ api/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace api
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+			using (WebApp.Start<Startup> ("http://localhost:5000")) {
+				
+				Console.WriteLine ("Server started");
+				Console.ReadLine ();
+			}
+		}
+	}
+}
