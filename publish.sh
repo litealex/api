3583cdf07f86179f623822ef88cef11e56d29a81
litealex <litealex@gmail.com>
2015-11-04 21:02:10 +0300

    publish sh

---
+++ publish.sh
@@ -0,0 +1,2 @@
+docker build -t docker3.cloudapp.net:5000/api .
+docker push docker3.cloudapp.net:5000/api
--- start.sh
+++
@@ -1,6 +0,0 @@
-cd /tmp
-git clone https://github.com/litealex/api.git
-cd api
-nuget restore
-xbuild
-mono /tmp/api/api/bin/Debug/api.exe
